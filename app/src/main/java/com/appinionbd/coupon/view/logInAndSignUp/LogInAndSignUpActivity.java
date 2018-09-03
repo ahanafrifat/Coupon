362eaa33e548b2ca446a06c0d606acfbbc2ea1d8
unknown <ahanafrifat@gmail.com>
2018-09-03 11:42:43 +0600

    Initial commit

---
+++ app/src/main/java/com/appinionbd/coupon/view/logInAndSignUp/LogInAndSignUpActivity.java
@@ -0,0 +1,15 @@
+package com.appinionbd.coupon.view.logInAndSignUp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appinionbd.coupon.R;
+
+public class LogInAndSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_and_sign_up);
+    }
+}
